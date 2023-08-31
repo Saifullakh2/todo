ce35e56e006f4aef5e30e487091319c6d9a4581e
Saifullakh1 <saifullakh7@gmail.com>
2023-08-31 19:17:48 +0600

    some changes

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from tasks.models import Task
 
 
@@ -8,3 +8,36 @@ def get_list(request):
         return render(request, 'index.html', {"tasks": tasks})
 
 
+def create_task(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        created_at = request.POST.get('created_at')
+        type = request.POST.get('type')
+        task = Task.objects.create(title=title, description=description, created_at=created_at, type=type)
+        return redirect('index')
+    return render(request, 'index.html')
+
+
+def update_task(request, id):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        created_at = request.POST.get('created_at')
+        type = request.POST.get('type')
+        task = Task.objects.get(id=id)
+        task.title = title
+        task.description = description
+        task.created_at = created_at
+        task.type = type
+        task.save()
+        return redirect('index')
+    return render(request, 'update.html')
+
+
+def delete_task(request, id):
+    if request.method == 'POST':
+        task = Task.objects.get(id=id)
+        task.delete()
+        return redirect('index')
+    return render(request, 'index.html')
